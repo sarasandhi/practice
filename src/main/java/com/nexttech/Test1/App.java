16dbba107ed2cf80bf137d685ad1784b85352ad7
sara <sara.eco38@gmail.com>
2022-07-14 15:26:03 -0700

    practice

---
+++ src/main/java/com/nexttech/Test1/App.java
@@ -0,0 +1,13 @@
+package com.nexttech.Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
